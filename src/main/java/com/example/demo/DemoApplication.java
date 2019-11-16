8fea06539fa20691097a6128cd485d3032c15b27
xiaokun-wsk <55736160+xiaokun-wsk@users.noreply.github.com>
2019-11-16 09:59:07 +0800

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
-@MapperScan(value = "com.example.demo.mapper")
+@MapperScan(value = "com.example.demo.mapper")//一定要配置mapper扫描否则会报错
 public class DemoApplication {
 
     public static void main(String[] args) {
